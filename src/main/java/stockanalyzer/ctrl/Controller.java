fc72fc37c24a1d88545f25a23c9c8d59cc22069c
Dr. Schluk <lukas.schelepet@aon.at>
2022-05-29 18:27:04 +0200

    v2 - implemented YahooFinance API and used Streams to analyze the Data

--- src/main/java/stockanalyzer/ctrl/Controller.java
+++ src/main/java/stockanalyzer/ctrl/Controller.java
@@ -2,39 +2,39 @@ package stockanalyzer.ctrl;
 
 import yahooApi.YahooFinance;
 import yahooApi.beans.QuoteResponse;
+import yahooApi.beans.Result;
 import yahooApi.beans.YahooResponse;
 
 import java.util.*;
 
 public class Controller {
-	YahooFinance yahooFinance = new YahooFinance();
-	List<String> tickers = Arrays.asList("AAPL", "AMZN", "TSLA", "GOOG", "OMV.VI",
-			"EBS.VI","DOC.VI","SBO.VI","RBI.VI","VIG.VI","TKA.VI","VOE.VI","FACC.VI","ANDR.VI","VER.VI",
-			"WIE.VI","CAI.VI","BG.VI","POST.VI","LNZ.VI","UQA.VI","SPI.VI","ATS.VI","IIA.VI");
 	public void process(String ticker) {
 		System.out.println("Start process");
-
-
+		//1) Daten laden
+		QuoteResponse data = (QuoteResponse) getData(ticker);
+		//2) Daten Analyse
 		try {
-			getData(ticker);	//1) Daten laden
-		} catch (Exception e) {
+			data.getResult().stream().forEach(quote -> System.out.println(quote.getSymbol() + ": " + quote.getAsk()));
+			System.out.println();
+
+			System.out.println("Highest value: " + data.getResult().stream().mapToDouble(Result::getAsk).max().orElseThrow(null));
+			System.out.println("Average value: " + data.getResult().stream().mapToDouble(Result::getAsk).average().orElseThrow(null));
+		} catch (RuntimeException e) {
 			e.printStackTrace();
 		}
 
-
-		//2) Daten Analyse
-		//Was soll man analysieren??? Angabe unschlüssig
 	}
 	
 
-	public Object getData(String searchString /*Wozu? Ich kann mit dem doch keine get-Funktionen rausfiltern oder?*/) {
-		//String data = yahooFinance.requestData(tickers);
-		YahooResponse response = yahooFinance.getCurrentData(tickers);
-		QuoteResponse quotes = response.getQuoteResponse();
-		System.out.println("Aktienkurse: " + quotes.getResult().stream().count());
-		System.out.println("Aktienkurse mit \"A\" im Symbol: " + quotes.getResult().stream().filter(result -> result.getSymbol().contains("A")).count());
-		System.out.println("Symbol\t\tDay High\t\t50 Day Avrg\t\t");
-		quotes.getResult().forEach(quote -> System.out.println(quote.getSymbol() + ":\t\t" + quote.getRegularMarketDayHigh() + " \t\t" +  quote.getFiftyDayAverage()));
+	public Object getData(String searchString) {
+		try {
+			List<String> tickerStringList = Arrays.asList(searchString.split(";"));
+			YahooFinance yahooFinance = new YahooFinance();
+			YahooResponse yahooResponse = yahooFinance.getCurrentData(tickerStringList);
+			return yahooResponse.getQuoteResponse();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
--- src/main/java/stockanalyzer/ui/UserInterface.java
+++ src/main/java/stockanalyzer/ui/UserInterface.java
@@ -4,6 +4,10 @@ package stockanalyzer.ui;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 import stockanalyzer.ctrl.Controller;
 
@@ -13,32 +17,40 @@ public class UserInterface
 	private Controller ctrl = new Controller();
 
 	public void getDataFromCtrl1(){
-		ctrl.process("ABC");
+		ctrl.process("AAPL;AMZN;TSLA;GOOG");
 	}
 
-	public void getDataFromCtrl2(){
+	public void getDataFromCtrl2(){ ctrl.process("OMV.VI;EBS.VI;DOC.VI;SBO.VI;RBI.VI;VIG.VI;TKA.VI");
 	}
 
-	public void getDataFromCtrl3(){
+	public void getDataFromCtrl3(){ ctrl.process("VOE.VI;FACC.VI;ANDR.VI;VER.VI;WIE.VI;CAI.VI;BG.VI;");
 
 	}
-	public void getDataFromCtrl4(){
+	public void getDataFromCtrl4(){ ctrl.process("POST.VI;LNZ.VI;UQA.VIM;SPI.VI;ATS.VI;IIA.VI");
 
 	}
 	
 	public void getDataForCustomInput() {
-		
+		System.out.println("\nPlease enter assets' symbols. Max 5 assets!");
+		Scanner scan = new Scanner(System.in);
+		List<String> symbolsToSearch = new ArrayList<>();
+		String symbolsString = "";
+		for(int i = 0; i < 5; i++) {
+			symbolsToSearch.add(scan.next());
+			symbolsString = symbolsString.concat(symbolsToSearch.get(i) + ";");
+		}
+		ctrl.process(symbolsString);
 	}
 
 
 	public void start() {
-		Menu<Runnable> menu = new Menu<>("User Interfacx");
+		Menu<Runnable> menu = new Menu<>("User Interface");
 		menu.setTitel("Wählen Sie aus:");
 		menu.insert("a", "Choice 1", this::getDataFromCtrl1);
 		menu.insert("b", "Choice 2", this::getDataFromCtrl2);
 		menu.insert("c", "Choice 3", this::getDataFromCtrl3);
-		menu.insert("d", "Choice User Imput:",this::getDataForCustomInput);
-		menu.insert("z", "Choice User Imput:",this::getDataFromCtrl4);
+		menu.insert("d", "Choice 4:",this::getDataFromCtrl4);
+		menu.insert("z", "Choice User Input:",this::getDataForCustomInput);
 		menu.insert("q", "Quit", null);
 		Runnable choice;
 		while ((choice = menu.exec()) != null) {
